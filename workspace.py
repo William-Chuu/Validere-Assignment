659e3b84aefa7cdedfbb5ab226e294d9d9fbbedb
William Chu <william_chu1998@hotmail.com>
2021-02-04 21:26:41 -0500

    successful plot of data

--- workspace.py
+++ workspace.py
@@ -2,11 +2,19 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
-from matpl
+from matplotlib import pyplot
 
 url = "https://www.crudemonitor.ca/crudes/dist.php?acr=MSW&time=recent"
+
 def main(url1, vol1, url2, vol2):
-    
+    x = data(url)
+    x.plot.scatter('percents', 'temp')
+    pyplot.show()
+
+
+# objective function to fit a line to data
+def objective(y, m, x, b):
+    return(m * x + b)
 
 
 
@@ -39,9 +47,16 @@ def data(url):
     
     # print (temp)
     # print (percents)
-    d = {'percents': percents, 'temp': temp}
 
-    return(pd.DataFrame(d))
-    # print(stats)
+    # create dict to be able to make into pandas
+    d = {'percents': percents, 'temp': temp}
+    df = pd.DataFrame(d)
+    # remove rows with '-'
+    df = df[df.temp != '-']
+    # convert all data types to floats
+    df = df.astype(float)
+    # print(df['temp'].dtypes)
+    return(df)
     
-data(url)
+main(url, 0, 0, 0)
+
