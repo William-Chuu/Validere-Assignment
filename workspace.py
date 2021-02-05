5d7f3bc1693483ec68c232b7941ff320b7025e3e
William Chu <william_chu1998@hotmail.com>
2021-02-04 23:57:45 -0500

    calc mixture m, b and data points

--- workspace.py
+++ workspace.py
@@ -14,13 +14,30 @@ def objective(m, x, b):
     return(m * x + b)
 
 def main(url1, vol1, url2, vol2):
+    tot_vol = vol1 + vol2
+    oil1_factor = vol1 / tot_vol
+    oil2_factor = vol2 / tot_vol
+
     oil1, oil2 = data(url1), data(url2)
+
     # choose the input and output variables
     x1, y1 = oil1['percents'], oil1['temp']
     x2, y2 = oil2['percents'], oil2['temp']
-    print(equation(x1, y1))
-    print(equation(x2, y2))
+    m1, b1 = LOBF(x1, y1)
+    m2, b2 = LOBF(x2, y2)
+    print(m1,b1,m2,b2)
+
+    mixture_m = (oil1_factor * m1) + (oil2_factor * m2)
+    mixture_b = (oil1_factor * b1) + (oil2_factor * b2)
+
+    mixture_df = pd.DataFrame({'percents': [5,10,20,30,40,50,60,70,80,90,95,99], 'temp': [0,0,0,0,0,0,0,0,0,0,0,0]})
+    mixture_df['temp'] = mixture_df.apply(lambda x: new_temps(x['percents'], mixture_m, mixture_b), axis=1)
+    print(mixture_df)
+
+
+
     
+
     # # plot input vs output
     # pyplot.scatter(x, y)
     # # define a sequence of inputs between the smallest and largest known inputs
@@ -31,15 +48,19 @@ def main(url1, vol1, url2, vol2):
     # pyplot.plot(x_line, y_line, '--', color='red')
     # pyplot.show()
 
+# calculates new temp of mixture
+def new_temps(percent, m, b):
+    return((m * percent) + b)
+
 # takes x and y data as params
-# returns string of curve fit equation
-def equation(x, y):
+# returns string of curve fit slope and y-int
+def LOBF(x, y):
     # curve fit
     # popt is type N-dimensional array (ndarray) with the optimal m and b values
     popt, _ = curve_fit(objective, x, y)
     # summarize the parameter values
     m, b = popt
-    return ('y = %.5f * x + %.5f' % (m, b))
+    return (m, b)
 
 # scrapes the URL for percentages and temps
 # returns as panda
@@ -82,5 +103,5 @@ def data(url):
     return(df)
 
 # data(url1)
-main(url1, 0, url2, 0)
+main(url1, 1, url2, 1)
 
